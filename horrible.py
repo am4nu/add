4f9d5422e8d730d6b0db63250b823174a54e5eb2
amaan uddin <md.amanuddin@gmail.com>
2019-11-26 08:21:19 -0500

    p to c

--- horrible.py
+++ horrible.py
@@ -1,171 +1,234 @@
+#include <iostream>
+#include <string>
+#include <limits>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <cstring>
+#include <climits>
+#define M 1000000007 
+using namespace std;
+    // #define PR(x) cout << #x " = " << x << "\n";
 
-#define variables globally so that it can be accessed from any other function
+int64_t arr[100007];
+int64_t arr2[100007];
+int64_t tree[262220];
+int64_t lazy[262220];
+int64_t lazyi[262220];
+void build(int64_t node, int64_t start, int64_t end)
+{
 
-arr=[]
-bui=[]
-lazyT=[]
-lazyA=[]
-M=10**9+7
-def AssUpdate(root,start,end,range_start,range_end,inc):
-	if start > end: #base case return from recursion
-		return
-	if lazyT[root]!=0:#before going further down the tree update the tree
-		bui[root]+=lazyT[root]*(end-start+1)#update the actual node
-		bui[root]%(M)
-		if(start!=end):#only you are not the end of the node push a littl further down
-			lazyT[root*2+1]+=lazyT[root]
-			lazyT[root*2+2]+=lazyT[root]
-			lazyT[root]=0 #this node is not lazy anymore
-	if lazyA[root]!=0:#before going further down the tree update the tree
-		bui[root]=lazyA[root]*(end-start+1)#update the actual node
-		bui[root]%(M)
-		if(start!=end):#only you are not the end of the node push a littl further down
-			lazyA[root*2+1]=lazyA[root]
-			lazyA[root*2+2]=lazyA[root]
-			lazyA[root]=0 #this node is not lazy anymore
-	if (start > range_end or end < range_start):
-		 return #no overlap return from this brance of the 
-	if start >= range_start and end <= range_end:
-		bui[root]=inc*(end-start+1)
-		bui[root]=bui[root]%M
-		if (start!=end):
-			lazyT[root*2+1]=inc;
-			lazyT[root*2+2]=inc;
-		return
+    if(start == end)
+    {
+        // Leaf node will have a single element
+        tree[node] = arr[end];
+    }
+    else
+    {
+        int64_t mid = (start + end) / 2;
+        // Recurse on the left child
+        build(2*node+1, start, mid);
+        // Recurse on the right child
+        build(2*node+2, mid+1, end);
+        // int64_ternal node will have the sum of both of its children
+        tree[node] = tree[2*node+1] + tree[2*node+2];
+        tree[node]=tree[node]%M;
+       
+    }
+}
+
+void sumUpdate(int64_t node, int64_t start, int64_t end, int64_t l, int64_t r, int64_t val)
+{
+    if(lazy[node] != 0)
+    { 
+        // This node needs to be updated
+        tree[node] += (end - start + 1) * lazy[node];    // Update it
+        if(start != end)
+        {
+            lazy[node*2+1] += lazy[node];                  // Mark child as lazy
+            lazy[node*2+2] += lazy[node];                // Mark child as lazy
+        }
+        lazy[node] = 0;                                  // Reset it
+    }
+      if(lazyi[node] != 0)
+    { 
+        // This node needs to be updated
+        tree[node] = (end - start + 1) * lazyi[node];    // Update it
+        if(start != end)
+        {
+            lazyi[node*2+1] = lazyi[node];                  // Mark child as lazy
+            lazyi[node*2+2] = lazyi[node];                // Mark child as lazy
+        }
+        lazyi[node] = 0;                                  // Reset it
+    }
+    
+    if(start > end or start > r or end < l)              // Current segment is not within range [l, r]
+        return;
+    if(start >= l and end <= r)
+    {
+        // Segment is fully within range
+        tree[node] += (end - start + 1) * val;
+        if(start != end)
+        {
+            // Not leaf node
+            lazy[node*2+1] += val;
+            lazy[node*2+2] += val;
+        }
+        return;
+    }
+    
+    
+    int64_t mid = (start + end) / 2;
+    sumUpdate(node*2 + 1, start, mid, l, r, val);        // Updating left child
+    sumUpdate(node*2 + 2, mid + 1, end, l, r, val);   // Updating right child
+    tree[node] = tree[node*2+1] + tree[node*2+2]; 
+    tree[node] =tree[node]%M;       // Updating root with max value 
+}
+void assUpdate(int64_t node, int64_t start, int64_t end, int64_t l, int64_t r, int64_t val)
+{
+    if(lazy[node] != 0)
+    { 
+        // This node needs to be updated
+        tree[node] += (end - start + 1) * lazy[node];    // Update it
+        if(start != end)
+        {
+            lazy[node*2+1] += lazy[node];                  // Mark child as lazy
+            lazy[node*2+2] += lazy[node];                // Mark child as lazy
+        }
+        lazy[node] = 0;                                  // Reset it
+    }
+      if(lazyi[node] != 0)
+    { 
+        // This node needs to be updated
+        tree[node] =  (end - start + 1)*lazyi[node];    // Update it
+        if(start != end)
+        {
+            lazyi[node*2+1] = lazyi[node];                  // Mark child as lazy
+            lazyi[node*2+2] = lazyi[node];                // Mark child as lazy
+        }
+        lazyi[node] = 0;                                  // Reset it
+    }
+    
+    if(start > end or start > r or end < l)              // Current segment is not within range [l, r]
+        return;
+    if(start >= l and end <= r)
+    {
+        // Segment is fully within range
+         tree[node] =  (end - start + 1)*val;
+        if(start != end)
+        {
+            // Not leaf node
+            lazy[node*2+1] = val;
+            lazy[node*2+2] = val;
+        }
+        return;
+    }
+    
+    
+    int64_t mid = (start + end) / 2;
+    assUpdate(node*2+1, start, mid, l, r, val);        // Updating left child
+    assUpdate(node*2 + 2, mid + 1, end, l, r, val);   // Updating right child
+    tree[node] = tree[node*2+1] + tree[node*2+2]; 
+    tree[node] =tree[node]%M;       // Updating root with max value 
+}
+
+
+
+int64_t queryRange(int64_t node, int64_t start, int64_t end, int64_t l, int64_t r)
+{
+    if(start > end || start > r || end < l)
+        return 0;         // Out of range
+    if(lazy[node] != 0)
+    {
+        // This node needs to be updated
+        tree[node] += (end - start + 1) * lazy[node];            // Update it
+        if(start != end)
+        {
+            lazy[node*2+1] += lazy[node];         // Mark child as lazy
+            lazy[node*2+2] += lazy[node];    // Mark child as lazy
+        }
+        lazy[node] = 0;                 // Reset it
+    }
+       if(lazyi[node] != 0)
+    { 
+        // This node needs to be updated
+        tree[node] =(end - start + 1)*lazyi[node];   // Update it
+        if(start != end)
+        {
+            lazyi[node*2+1] = lazyi[node];                  // Mark child as lazy
+            lazyi[node*2+2] = lazyi[node];                // Mark child as lazy
+        }
+        lazyi[node] = 0;                                  // Reset it
+    }
+    if(start >= l && end <= r)             // Current segment is totally within range [l, r]
+        return tree[node];
+    int64_t mid = (start + end) / 2;
+    int64_t p1 = queryRange(node*2+1, start, mid, l, r);         // Query left child
+    int64_t p2 = queryRange(node*2 + 2, mid + 1, end, l, r); // Query right child
+    return (p1 + p2)%M;
+}
+
+int main()
+{
+	 ios_base::sync_with_stdio(false);
+	int64_t inst,start,end,inc,N,K,r_start,r_end;
+	
+	cin>>N>>K;
+	for( int64_t i=0;i<N;i++){
+	cin>>arr[i];
+	cin>>arr2[i];
+	} //for test
+	//for ( int64_t j=0;j<N;j++)
+	//cout<<arr[j];
+	int64_t node=0;
+	start=0,end=N-1;
+	build(node,start,end);
+	
+	for(int64_t i=0;i<K;i++)
+	{
+		cin>>inst >> r_start >> r_end;
+		r_start=r_start-1;
+		r_end=r_end-1;
+		if(inst==4){
+		int64_t sum = 0;
+for (int64_t i = r_start; i <= r_end; i++)
+	sum += arr2[i];
+	sum %= M;
+	int64_t sum2 =queryRange(node,start,end,r_start,r_end)%M;	
+		if(sum!=sum2) cout <<"error"<<sum<<" "<<sum2<<endl;
+		}
 		
-	AssUpdate(root*2+1,start,(start+end)//2,range_start,range_end,inc)
-	AssUpdate(root*2+2,(start+end)//2+1,end,range_start,range_end,inc)
-	bui[root]=bui[root*2+1] + bui[root*2+2]
-	bui[root]%(M)
+	else if(inst==1){
+		cin >>inc;
+	for (int64_t i = r_start; i <= r_end; i++)
+	     arr2[i]+=inc;
+	 sumUpdate(node,start,end,r_start,r_end,inc);
+		
+	}
+	else if(inst==3){
+		cin >> inc;
+		for (int64_t i = r_start; i <= r_end; i++)
+	     arr2[i]=inc;
+		assUpdate(node,start,end,r_start,r_end,inc);
+		
+	}
+	else if(inst==2){
+		cin >>inc;
+		
+		
+	}
 			
-def SumUpdate(root,start,end,range_start,range_end,inc):
-	if start > end: #base case return from recursion
-		return
-	if lazyT[root]!=0:#before going further down the tree update the tree
-		bui[root]+=lazyT[root]*(end-start+1)#update the actual node
-		bui[root]%(M)
-		if(start!=end):#only you are not the end of the node push a littl further down
-			lazyT[root*2+1]+=lazyT[root]
-			lazyT[root*2+2]+=lazyT[root]
-			lazyT[root]=0 #this node is not lazy anymore
-	if lazyA[root]!=0:#before going further down the tree update the tree
-		bui[root]=lazyA[root]*(end-start+1)#update the actual node
-		bui[root]%(M)
-		if(start!=end):#only you are not the end of the node push a littl further down
-			lazyA[root*2+1]+=lazyA[root]
-			lazyA[root*2+2]+=lazyA[root]
-			lazyA[root]=0 #this node is not lazy anymore
-	if (start > range_end or end < range_start):
-		 return #no overlap return from this brance of the 
-	if start >= range_start and end <= range_end:
-		bui[root]+=inc*(end-start+1)
-		bui[root]=bui[root]%M
-		if (start!=end):
-			lazyT[root*2+1]+=inc;
-			lazyT[root*2+2]+=inc;
-		return
-		
-	SumUpdate(root*2+1,start,(start+end)//2,range_start,range_end,inc)
-	SumUpdate(root*2+2,(start+end)//2+1,end,range_start,range_end,inc)
-	bui[root]=bui[root*2+1] + bui[root*2+2]
-	bui[root]%(M)
-			
-def SumQuery(root,start,end,range_start,range_end):
-	
-	if start > end: # no overlap or base condition
-		return 0
-	
-	if (lazyT[root]!= 0):
-		bui[root]+=lazyT[root]*(end-start+1)
-		bui[root]%(M)
-		if start != end:
-			lazyT[root*2+1]+=lazyT[root]
-			lazyT[root*2+2]+=lazyT[root]
-		lazyT[root]=0
-	if lazyA[root]!=0:#before going further down the tree update the tree
-		bui[root]=lazyA[root]*(end-start+1)#update the actual node
-		bui[root]%(M)
-		if(start!=end):#only you are not the end of the node push a littl further down
-			lazyA[root*2+1]+=lazyA[root]
-			lazyA[root*2+2]+=lazyA[root]
-		lazyA[root]=0 #this node is not lazy anymore
-	if end < range_start or start > range_end:
-		return 0
-	if start >= range_start and end <= range_end:
-		return bui[root]
-	q1=SumQuery(root*2+1,start, (start+end)//2,range_start,range_end)
-	q1=q1%(M)
-	q2=SumQuery(root*2+2,((start+end)//2) +1,end,range_start,range_end)
-	q2=q2%(M)
-	return int(q1+q2)%(M)
-				
-
-def build(root,start,end): #recursive build tree function
-
-	if(start!=end):
-		bui[root]=build(root*2+1,start,(start+end)//2)+build(root*2+2,(start+end)//2+1,end)
-		#make a recursive call to sum the values of the two child node
-		bui[root]=bui[root]%(M)
-		#print("i am here")
-		return int(bui[root]%(M))#return the value at the node root
-	if(start==end):#base case if the node is the leaf just assign the value of the array
-		#print("i am here root and start",root,start)
-		bui[root]=arr[start]
-		return int(bui[root]%(M))
-  
-
- #n=int(input("enter the number of elements in the array"))
-try:
-	N,Q = map(int, raw_input().split())
-
-	#print(N,Q)
-	arr=[0]*N
-	arr[1:] = list(map(int, raw_input().split()))
-	print(arr)
-	q=Q
-	n=N
- #arr=[0]*n #create an array to hold n number of elements in the array
- #for i in range(n): #run the loop to 
-  #arr[i]=int(input())#take input and assign it to the array
-
-
-	bui=[0]*int(2*n)#if n is number of leaf then total number of nodes is  2*n-1 
-	lazyT=[0]*int(2*n)
-	lazyA=[0]*int(2*n)
-	root=1 #tree node root
-	start=1
-	en=n#last member of array location
-	sucess=build(root,start,en)#call the function
-	#print("build works")
-#SumUpdate(root,start,en,0,1,1)
-#print("sumquery",SumQuery(root,start,en,0,1))
-#print(bui)
-	#print(bui)
-
-	for i in range(q):
-		print(bui)
-		qu = list(map(int, raw_input().split()))
-
-		if(qu[0]==4):
-			print(int(SumQuery(root,start,en,(qu[1]),(qu[2]))%(M)))
-		elif(qu[0]==3):
-			AssUpdate(root,start,en,(qu[1]),(qu[2]),qu[3])
-		elif(qu[0]==1):
-			SumUpdate(root,start,en,(qu[1]),(qu[2]),qu[3])
-		else:
-			zebra=1
-except:
-	pass
-		
-	
-
-
-
-  
-  
-
-  
+	}
+return 0;	
+}
 	
 	
- 
-   
+	
+
